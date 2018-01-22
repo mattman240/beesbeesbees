909306e46fa44531c2c809125294c107b965e2a9
mattman240 <mattmull98@gmail.com>
2018-01-22 11:34:05 -0800

    finished all the bee constructors

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  HoneyMakerBee.call(this);
+  this.job = 'find pollen';
+  this.age = 10;
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype)
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure)
+}
+
+ForagerBee.prototype.constructor = ForagerBee;
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype)
 
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead'
+}
+
+RetiredForagerBee.prototype.gamble = function(winnings) {
+  this.treasureChest.push(winnings)
+}
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
